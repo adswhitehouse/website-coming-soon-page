b7f280a08252301f2019f55853ca9ae6464827a0
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-02 21:56:45 +0000

    made countdown responsive and animated rocket image

---
+++ script.js
@@ -0,0 +1,39 @@
+// Variables
+let daysNumber = document.querySelector(".jsDaysNumber");
+let hoursNumber = document.querySelector(".jsHoursNumber");
+let minutesNumber = document.querySelector(".jsMinutesNumber");
+let secondsNumber = document.querySelector(".jsSecondsNumber");
+
+// Website launch time in milliseconds
+let futureTime = new Date(2025, 5, 19, 0).getTime();
+
+// Function to append a 0 if time remaining is less than 10 for style and readability
+function updateDate(number, variable) {
+  if (number < 10) {
+    variable.textContent = `0${number}`;
+  } else {
+    variable.textContent = number;
+  }
+}
+
+setInterval(() => {
+  // Current time in milliseconds
+  let currentTime = Date.now();
+
+  // Remaining milliseconds between events
+  let remainingTime = futureTime - currentTime;
+
+  // Calculates days/hours/minutes/seconds remaining in remainingTime
+  let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+  // Updates page
+  updateDate(days, daysNumber);
+  updateDate(hours, hoursNumber);
+  updateDate(minutes, minutesNumber);
+  updateDate(seconds, secondsNumber);
+}, 1000);
